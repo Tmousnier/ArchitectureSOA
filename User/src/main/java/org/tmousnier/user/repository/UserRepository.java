d1b61d8d3613ca764c0853db7c1a4744815fcebc
Theo Mousnier <mousniert.pro@gmail.com>
2025-06-16 11:59:06 +0200

    Reposiotry update user

--- User/src/main/java/org/tmousnier/user/repository/UserRepository.java
+++ User/src/main/java/org/tmousnier/user/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package org.tmousnier.user.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.tmousnier.user.dao.User;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, String> {
 }
