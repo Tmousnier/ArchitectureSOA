83be24eefec36f74628672cc618e1d91849eb259
Theo Mousnier <mousniert.pro@gmail.com>
2025-06-16 11:58:48 +0200

    Dto update UserInput

--- User/src/main/java/org/tmousnier/user/dto/UserInput.java
+++ User/src/main/java/org/tmousnier/user/dto/UserInput.java
@@ -1,4 +1,12 @@
 package org.tmousnier.user.dto;
 
-public class UserInput {
+public record UserInput (String email, String password) {
+    public UserInput {
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("Email cannot be null or blank");
+        }
+        if (password == null || password.isBlank()) {
+            throw new IllegalArgumentException("Password cannot be null or blank");
+        }
+    }
 }
