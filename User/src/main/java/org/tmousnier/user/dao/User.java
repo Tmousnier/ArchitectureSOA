4cbf72727d2239ba1933c8f103d02af7af2b5b6b
Theo Mousnier <mousniert.pro@gmail.com>
2025-06-16 11:58:10 +0200

    DAO update user

--- User/src/main/java/org/tmousnier/user/dao/User.java
+++ User/src/main/java/org/tmousnier/user/dao/User.java
@@ -1,4 +1,22 @@
 package org.tmousnier.user.dao;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @UuidGenerator
+    private String uuid;
+    private String email;
+    private String password;
 }
